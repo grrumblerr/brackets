0392b4038afa8763fc62f804233d8f75dbe704ac
grrumblerr <grrumblerr@yandex.by>
2022-02-09 19:12:14 +0300

    refactor

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-
+ 
   let pairs = Object.fromEntries(bracketsConfig.map(([i,j])=>[j,i]));
   let opens = Object.values(pairs);
   let closed = Object.keys(pairs);
-  // let string = str.split('');
   let stack = [];
  
 
@@ -26,20 +24,5 @@ module.exports = function check(str, bracketsConfig) {
       }
     }
   }
-    // return stack;
   return stack.length===0;
 }
-
-// console.log(check('|()|(||)||', [['(', ')'], ['|', '|']]));
-
-// const config3 = [['(', ')'], ['[', ']'], ['{', '}']];
-// const config6 = [['1', '2'], ['3', '4'], ['5', '6'], ['7', '7'], ['8', '8']];
-// const config7 = [['(', ')'], ['[', ']'], ['{', '}'], ['|', '|']];
-// let stringg = '([{}])';
-// console.log(stringg.split(''));
-
-// let pairs = Object.fromEntries(config6.map(([i,j])=>[j,i]));
-// console.log(pairs);
-// console.log(pairs['8'])
-// let opens = Object.values(pairs);
-// console.log(opens);
